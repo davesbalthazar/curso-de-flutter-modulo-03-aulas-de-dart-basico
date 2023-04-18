65031e0e8d82d529a56048da6dcb5e06c2fcfdd3
Daves Balthazar <51927678+davesbalthazar@users.noreply.github.com>
2023-04-18 10:23:52 -0300

    Aula 8

---
+++ dart-aula-8-if-avancado-no-dart-com-operador-ternario.dart
@@ -0,0 +1,24 @@
+///
+/// Curso de Flutter e Dart
+/// Daves Balthazar
+/// Aula 8 - Operador Ternário no Dart
+/// https://www.youtube.com/watch?v=3lv9kLK89wc&list=PL5EmR7zuTn_Yu_YV2pT0h0843vRGiTMtx&index=11
+///
+void main() {
+  bool condicao = false;
+  String nome = 'Pedro';
+  String clima = 'Chuva';
+
+//  print( condicao ? 'Condição Verdadeira':'Condição Falsa' );
+
+//  print( 2 > 3 ? 'Menor':'Maior ou igual' );
+
+//  print( nome == 'Daves' ? 'Nome OK':'Nome Errado' );
+
+//  String cliente = nome == 'Daves' ? 'Nome OK':'Nome Errado';
+//  print( cliente );
+
+  String mensagem =
+      clima == 'Sol' ? 'Lindo dia ensolarado!' : 'Tomara que saia sol';
+  print(mensagem);
+}
