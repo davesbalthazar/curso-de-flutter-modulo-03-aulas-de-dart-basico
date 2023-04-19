e5afd83c57adc8715249fb30be7d5840c4242cd0
Daves Balthazar <51927678+davesbalthazar@users.noreply.github.com>
2023-04-19 12:42:09 -0300

    Aula 5 - Incremento de Variáveis e Operadores no Dart ++

--- dart-aula-5-incremento-de-variaveis.dart
+++ dart-aula-5-incremento-de-variaveis.dart
@@ -5,21 +5,18 @@
 /// https://www.youtube.com/watch?v=0gN8tV1AFzE&list=PL5EmR7zuTn_Yu_YV2pT0h0843vRGiTMtx&index=8
 ///
 void main() {
-
   int numero;
-  
+
   numero = 10;
   //numero = numero + 1;
-  
   //numero++;    // numero = numero + 1
   //numero--;    // numero = numero - 1
   //numero+=2;     // numero = numero + 2
   //numero-=2;   // numero = numero - 2
-  
+
   //++numero;
-  
-  print( numero-- );
-  
-  print( numero );
-    
+
+  print(numero--);
+
+  print(numero);
 }
