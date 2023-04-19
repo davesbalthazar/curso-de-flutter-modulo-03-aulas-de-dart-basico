4e94d25b6995fc29719b50110844abf5b7c4142c
Daves Balthazar <51927678+davesbalthazar@users.noreply.github.com>
2023-04-19 12:42:40 -0300

    Aula 6 - Decisões no Dart - if e else

--- dart-aula-6-if-else-no-dart.dart
+++ dart-aula-6-if-else-no-dart.dart
@@ -5,23 +5,20 @@
 /// https://www.youtube.com/watch?v=d6FqxbnlJGw&list=PL5EmR7zuTn_Yu_YV2pT0h0843vRGiTMtx&index=10
 ///
 void main() {
-
+  // Decisões no Dart - if e else
   int numero1 = 10;
   int numero2 = 10;
-  
-  print( 'numero1 $numero1, numero2 $numero2');
-   
-  
-  if( numero1 == numero2 ) {
-    print( 'numero1 é igual ao numero2');    
-  } else if( numero1 > numero2 ) {
-      print( 'numero1 é maior do numero2');        
-    } else {
-      print( 'numero1 é menor do numero2');        
-    
-    }
-  
-  
+
+  print('numero1 $numero1, numero2 $numero2');
+
+  if (numero1 == numero2) {
+    print('numero1 é igual ao numero2');
+  } else if (numero1 > numero2) {
+    print('numero1 é maior do numero2');
+  } else {
+    print('numero1 é menor do numero2');
+  }
+
   /*
   if( numero1 == numero2 ) {
     print( 'numero1 é igual ao numero2');    
@@ -29,8 +26,7 @@ void main() {
     print( 'numero1 é diferente do numero2');        
   }
   */
-  
-  
+
   /*
   // Comparação de valores
   if( numero1 == numero2 ) {
